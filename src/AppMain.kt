53a208051d0c9b863ea9a124c6cb0388ad6f9be3
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2017-12-19 04:36:40 +0800

    Initial commit

---
+++ src/AppMain.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+    print("Enter Problem Number: ")
+
+    val (number) = readLine()!!.split(' ')
+
+    showSolution(number.toInt());
+}
+
+fun showSolution(problemNumber: Int){
+    println("No solution for this problem yet!")
+}
