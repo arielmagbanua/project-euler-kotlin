a754e7fc4b69ccdcf94669987dbfe5581b428706
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2017-12-19 21:48:01 +0800

    Added problem 1 solution.

--- src/AppMain.kt
+++ src/AppMain.kt
@@ -3,13 +3,20 @@ fun main(args: Array<String>){
 
     val (number) = readLine()!!.split(' ')
 
-    showSolution(number.toInt());
+    showSolution(number.toInt())
 }
 
 fun showSolution(problemNumber: Int){
 
     when(problemNumber){
-        1 -> print(problemNumber)
+        1 -> {
+            val problem = Problem1()
+            print("Enter a Number: ")
+            val(num) = readLine()!!.split(' ').map(String::toInt)
+
+            val answer = problem.solve(num)
+            println("Answer: $answer")
+        }
         else -> {
             println("No solution for this problem yet!")
         }
---
+++ src/Problem1.kt
@@ -0,0 +1,19 @@
+/**
+ * Multiples of 3 and 5
+ * Problem 1
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+class Problem1(){
+    fun solve(paramNum: Int): Int{
+        var sum: Int = 0
+        for(i in 1..paramNum){
+            if (i%3 == 0 || i%5 == 0){
+                sum += i
+            }
+        }
+
+        return sum
+    }
+}
